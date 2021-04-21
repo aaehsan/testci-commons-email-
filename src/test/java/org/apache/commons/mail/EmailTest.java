6c680cb0a9059cf5eca0a250bb9b549cffef9bc3
aminah ehsan <aaehsan@umich.edu>
2021-04-20 22:30:52 -0400

    correction

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -20,8 +20,8 @@ public class EmailTest {
 	private static final String[] TEST_EMAILS = { "ab@bc.com", "a.b@c.org",
 			"acbdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd"};
 	
-	private EmailConcrete email
-	private Date d = new Date(2021-25-4)
+	private EmailConcrete email;
+	private Date d = new Date(2021-25-4);
 	private int connectionS = 3;
 	private static final String emailName = "abc@gmail.com";
 	private static final String value = "eVal";
